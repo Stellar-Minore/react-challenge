cc687fdd7e34e14170dcd701c81c4691ede3c05f
Samrah Sajid <samrah.sajid@rekd-app.com>
2021-03-18 11:20:02 +0500

    Update app to add profile screen route

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
+import Profile from '../profile';
 
 function App() {
   return (
     <div>
       <Switch>
+        { /* profile screen */}
+        <Route path="/" exact><Profile /></Route>
       </Switch>
     </div>
   );
