548a9b4679111b02ce00a6a1641db56a26945b43
Samrah Sajid <samrah.sajid@rekd-app.com>
2021-03-18 11:18:59 +0500

    Create scaffolding for profile component

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from './profile.module.css';
+
+class Profile extends React.Component {
+  render() {
+    return (null)
+  }
+}
+
+export default Profile;
