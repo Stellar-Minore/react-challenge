166005f45d333fdd457816ed2562fa50eb6a255a
Arooba Shahoor <ashahoor@rekd-app.com>
2021-04-14 14:07:08 +0500

    Update image details to add content

--- src/components/imageDetails/imageDetails.jsx
+++ src/components/imageDetails/imageDetails.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import Styles from './imageDetails.module.css';
 
 function ImageDetails(props) {
+  const imageNumber = props.match.params.id;
+
+  if (!sessionStorage.getItem(`image-${imageNumber}`) && props.location.image) {
+    sessionStorage.setItem(`image-${imageNumber}`, props.location.image);
+  }
 
   return (
     <div className={Styles.mainContainer}>
+      <p>This is image number {imageNumber} in the grid</p>
+      <img src={sessionStorage.getItem(`image-${imageNumber}`)} alt={`Character # ${imageNumber}`} />
     </div>
   )
 }
