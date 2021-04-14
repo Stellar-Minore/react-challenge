634ef2929f45b5a0ca3ad4ae73aa8bf45ec3c7f0
Arooba Shahoor <ashahoor@rekd-app.com>
2021-04-14 14:06:35 +0500

    Create image details component scaffolding

---
+++ src/components/imageDetails/imageDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from './imageDetails.module.css';
+
+function ImageDetails(props) {
+
+  return (
+    <div className={Styles.mainContainer}>
+    </div>
+  )
+}
+
+export default ImageDetails;
